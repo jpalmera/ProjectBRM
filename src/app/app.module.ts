bcd72849279f81b9c53ffdff9faa47b97702f51d
jpalmera <jaanpalmera@gmail.com>
2019-07-16 22:24:13 -0500

    ProjectBRM
    Signed-off-by: jpalmera <jaanpalmera@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FootComponent } from './foot/foot.component';
+import { StructureComponent } from './structure/structure.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FootComponent,
+    StructureComponent
   ],
   imports: [
     BrowserModule,
